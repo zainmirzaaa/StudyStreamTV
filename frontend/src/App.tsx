291a83d34cd324dd6a17050b96431bc29c533fac
Vishnu Patel <viptl123@gmail.com>
2024-12-23 02:14:24 -0600

    added navigation pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import CreateAccountPage from './components/CreateAccountPage';
 import './App.css';
 import { AuthProvider } from './Context/AuthContext';
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Route path="/" element={<SignInPage/>} />
         <Route path="/create-account" element={<CreateAccountPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/stream" element={<Home />} />
+        <Route path="/watch" element={<Home />} />
       </Routes>
     </Router>
   
